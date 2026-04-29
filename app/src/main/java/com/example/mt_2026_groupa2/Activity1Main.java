51500caabb6d425ef8b0bf1c0275ace61c20c1c0
RadicalIridium <rylandvm@gmail.com>
2026-04-29 11:38:40 +1000

    29/04/2026 Added  parts of a framework for activity2 and empty act3(OpenCamera) and act4(LoadImage)

--- app/src/main/java/com/example/mt_2026_groupa2/Activity1Main.java
+++ app/src/main/java/com/example/mt_2026_groupa2/Activity1Main.java
@@ -8,7 +8,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class ActivityMain1 extends AppCompatActivity {
+public class Activity1Main extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
