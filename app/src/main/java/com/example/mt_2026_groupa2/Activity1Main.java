d6a695e35e40ff3ba627ebc06648d6f17b63768c
RadicalIridium <rylandvm@gmail.com>
2026-04-30 15:35:47 +1000

    30/04/2026 - Finished Act1, Act2... - Firebase Working - Open camera, and Load image working - All MLkit working Barcode, Context, Text - Small functions working, image changes, only one MLK called, invisible edit results till mlk run - Cleaned files

--- app/src/main/java/com/example/mt_2026_groupa2/Activity1Main.java
+++ app/src/main/java/com/example/mt_2026_groupa2/Activity1Main.java
@@ -3,7 +3,6 @@ package com.example.mt_2026_groupa2;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
@@ -13,6 +12,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class Activity1Main extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,10 +29,12 @@ public class Activity1Main extends AppCompatActivity {
         imageButtonBarcodeReader.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(Activity1Main.this, Activity2.class);
-                startActivity(intent);
+                Intent intent = new Intent(Activity1Main.this, Activity2MLK.class);
                 // add intent to change image
                 intent.putExtra("image_id", R.drawable.barcode);
+                // add intent to change selected MLK mode
+                intent.putExtra("MLKmode", 1);
+
                 startActivity(intent);
             }
         });
@@ -41,10 +43,12 @@ public class Activity1Main extends AppCompatActivity {
         imageButtonContentReader.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(Activity1Main.this, Activity2.class);
-                startActivity(intent);
+                Intent intent = new Intent(Activity1Main.this, Activity2MLK.class);
                 // add intent to change image
                 intent.putExtra("image_id", R.drawable.content);
+                // add intent to change selected MLK mode
+                intent.putExtra("MLKmode", 2);
+
                 startActivity(intent);
             }
         });
@@ -53,10 +57,12 @@ public class Activity1Main extends AppCompatActivity {
         imageButtonTextReader.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(Activity1Main.this, Activity2.class);
-                startActivity(intent);
+                Intent intent = new Intent(Activity1Main.this, Activity2MLK.class);
                 // add intent to change image
                 intent.putExtra("image_id", R.drawable.text);
+                // add intent to change selected MLK mode
+                intent.putExtra("MLKmode", 3);
+
                 startActivity(intent);
             }
         });
--- app/src/main/java/com/example/mt_2026_groupa2/Activity5Edit.java
+++ app/src/main/java/com/example/mt_2026_groupa2/Activity5Edit.java
@@ -1,7 +1,6 @@
 package com.example.mt_2026_groupa2;
 
 import android.os.Bundle;
-import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,27 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class Activity2 extends AppCompatActivity {
+public class Activity5Edit extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_2);
-
-        // Image Changer
-        int imageId = getIntent().getIntExtra("image_id", R.drawable.barcode);
-
-        ImageView imageView = findViewById(R.id.imageViewAct2Change);
-        imageView.setImageResource(imageId);
-
-
+        setContentView(R.layout.activity_activity5_edit);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-
-
         });
     }
 }
